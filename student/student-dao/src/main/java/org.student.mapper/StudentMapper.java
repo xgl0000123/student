cf539bf245911b82c89702257ec961e7659c5ab7
xgl0000123 <54236280+xgl0000123@users.noreply.github.com>
2019-08-18 20:33:51 +0800

    00

--- student/student-dao/src/main/java/org.student.mapper/StudentMapper.java
+++ student/student-dao/src/main/java/org.student.mapper/StudentMapper.java
@@ -2,7 +2,7 @@ package org.student.mapper;
 
 import org.springframework.stereotype.Repository;
 import org.student.entity.Student;
-//12345
+//0000
 @Repository
 public interface StudentMapper {
     Student selectByStuNo(int stuNo);
